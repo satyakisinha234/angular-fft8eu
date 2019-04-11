acec0222cf09ac7f245080a62874d518f80c2f5d
satyakisinha234 <satyakisinha234@gmail.com>
2019-04-11 09:23:11 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StudentdetailsComponentComponent } from './studentdetails-component/studentdetails-component.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule,RouterModule ],
   declarations: [ AppComponent, HelloComponent, StudentdetailsComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
