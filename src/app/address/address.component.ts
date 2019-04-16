ce575cb6e8a24b3d4e34cfe5123eaec58cc704e5
satyakisinha234 <satyakisinha234@gmail.com>
2019-04-16 02:27:52 -0400

    1 file modified, 1 added

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-address',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressComponent implements OnInit {
 
+  @Input() showDiv : boolean=false;
+  address1:string;
+  address2:string;
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
